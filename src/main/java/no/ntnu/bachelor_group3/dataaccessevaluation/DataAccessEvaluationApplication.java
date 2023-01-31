0c26adb947e94dfd2f402ff6251a71edd6986fc3
Jonas Klinge Husøy <jonaskh@stud.ntnu.no>
2023-01-31 08:48:31 +0100

    Initial commit with dependencies for spring data jpa, jdbc and spring boot

---
+++ src/main/java/no/ntnu/bachelor_group3/dataaccessevaluation/DataAccessEvaluationApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.bachelor_group3.dataaccessevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAccessEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/bachelor_group3/dataaccessevaluation/DataAccessEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.bachelor_group3.dataaccessevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
