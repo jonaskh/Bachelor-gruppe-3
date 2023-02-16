cb1cf4b62cde6c14e7b179c517e9b09c86378cb6
Jonas Klinge Husøy <jonaskh@stud.ntnu.no>
2023-02-16 15:19:57 +0100

    Changed JDK to 11

--- src/main/java/no/ntnu/bachelor_group3/dataaccessevaluation/Data/CustomerRepository.java
+++ src/main/java/no/ntnu/bachelor_group3/dataaccessevaluation/Data/CustomerRepository.java
@@ -1,6 +1,7 @@
 package no.ntnu.bachelor_group3.dataaccessevaluation.Data;
 
 import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,6 @@ import java.util.List;
 
 
 @Repository
-public interface CustomerRepository extends CrudRepository<customer, String> {
+public interface CustomerRepository extends JpaRepository<customer, String> {
 
 }
