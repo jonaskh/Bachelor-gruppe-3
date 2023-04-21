2ad735ec6f41b41ec98ef882b9f999217accfa21
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2023-04-21 13:28:10 +0200

    Added validPostalCodes datatype

--- src/main/java/no/ntnu/bachelor_group3/jdbcevaluation/Data/ValidPostalCode.java
+++ src/main/java/no/ntnu/bachelor_group3/jdbcevaluation/Data/ValidPostalCode.java
@@ -1,5 +1,34 @@
 package no.ntnu.bachelor_group3.jdbcevaluation.Data;
 
 public class ValidPostalCode {
+    private Long id;
 
+    private Terminal terminal;
+    private String area;
+
+    public ValidPostalCode(Long id, Terminal terminal, String area) {
+        this.id = id;
+        this.terminal = terminal;
+        this.area = area;
+    }
+
+    public Terminal getTerminal() {
+        return terminal;
+    }
+
+    public void setTerminal(Terminal terminal) {
+        this.terminal = terminal;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
 }
