f5f2745645fb0a7541e57393efd05d21ab717c60
Stian B Kaspersen <stianbka@stud.ntnu.no>
2023-05-13 16:44:36 +0200

    Added NTNU logo to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,9 @@ function App() {
         <>
 
             <div style={{ textAlign: "center", margin: "20px", padding: "20px" }}>
-                <h1>Assessing data access layers for:</h1>
+                <h1 style={{fontSize: "5rem", marginBottom: "25px"}}>Assessing data access layers</h1>
                 <img src={process.env.PUBLIC_URL + '/postnord.jpg'} alt="Postnord logo" />
+                <img style={{ width: "30%", marginLeft: "50px" }} src={process.env.PUBLIC_URL + '/NTNU-logo.svg.png'} alt="NTNU logo" />
             </div>
 
             <div style={{margin: "20px", padding: "10px" }}>
