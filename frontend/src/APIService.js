2bbbf0830560484151abbbe13c1cc67e5c0b9223
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2023-05-17 12:00:05 +0200

    Frontend update

--- frontend/src/APIService.js
+++ frontend/src/APIService.js
@@ -2,15 +2,30 @@ import axios from 'axios';
 
 const BACKEND_URL = "http://localhost:8080";
 const CUSTOMER_REST_API_URL = BACKEND_URL + "/customer";
+const TERMINAL_REST_API_URL = BACKEND_URL + "/terminal";
+const POSTAL_REST_APO_URL = BACKEND_URL + "/postalcode";
 
 class APIService {
+
     getCustomers() {
         return axios.get(CUSTOMER_REST_API_URL);
     }
 
-    /*addCustomers(newData) {
-        return axios.post(CUSTOMER_REST_API_URL, newData);
-    }*/
+
+    getTerminals() {
+        return axios.get(TERMINAL_REST_API_URL);
+    }
+
+
+    getShipmentEval() {
+        return axios.get(  BACKEND_URL + "/shipment/eval");
+    }
+
+    getCheckpointEval() {
+        return axios.get(  BACKEND_URL + "/checkpoint/eval");
+    }
+
+
 }
 
 export default new APIService();
